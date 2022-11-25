a8e4dcfa725835c8c76d7f9ad9531ed2c55738fd
Kai Takami <kaitakami@protonmail.com>
2022-11-25 20:58:53 +0900

    Add new, create methods to post new events

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,2 +1,36 @@
 class EventController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @friend = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(post_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html do
+          redirect_to event_url(@event), notice: "Event was created :)"
+        end
+        format.json { render :show, status: :unprocessable_entity }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:event).permit(:title, :description, :date, :public_event)
+  end
 end
